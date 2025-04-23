8fb02ca1bd7daf0f79ee3cadd2c8214864aef463
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-04-23 11:23:33 +0200

    ejercicioComentado

--- data.js
+++ data.js
@@ -1,33 +1,11 @@
-// requerir informacion al server
-//hacer html...buscar como!
-//crear la respuesta con res.end
+//crear constante con datos como si fuese un objeto
+//exportarlo con module.exports
 
 
-const http = require("http");
-const webInfo = require("./data");
-
-const server = http.createServer((req, res) => {
+const webInfo = {
+    title: "Ejercicio HTTP",
+    subtitle: "Espero que salga bien",
+    description: "Ejercicio de the Bridge para el backend",
+  };
   
-  const html = `
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${webInfo.title}</title>
-    </head>
-    <body>
-      <h1>${webInfo.title} Probando!!</h1>
-      <h2>${webInfo.subtitle}1,2,3</h2>
-      <p>${webInfo.description}</p>
-    </body>
-    </html>
-  `;
-
-  
-  res.end(html);
-});
-
-server.listen(0, () => {
-    console.log(`server listening on por http://localhost:${server.address().port}`);
-  });
+  module.exports = webInfo
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-//crear constante con datos como si fuese un objeto
-//exportarlo con module.exports
+// requerir informacion al server
+//hacer html...buscar como!
+//crear la respuesta con res.end
 
 
-const webInfo = {
-    title: "Ejercicio HTTP",
-    subtitle: "Espero que salga bien",
-    description: "Ejercicio de the Bridge para el backend",
-  };
+const http = require("node:http");
+const {title, subtitle, description} = require("./data.js");//hacemos destructuring para traernos los objetos del data server
+
+const server = http.createServer((req, res) => {
   
-  module.exports = webInfo
+  const html = `
+  <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}challenge</title>
+    </head>
+    <body>
+      <h1>${title}</h1>
+      <h2>${subtitle}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+  `;
+
+  
+  res.end(html);
+});
+
+const PORT = 3000 // se puede guardar el puerto en variable y luego poder cambiarlo aqui.
+
+server.listen(PORT, () => {
+    console.log(`server listening on por http://localhost:${PORT}`);//${server.address().port} esto seria para usarlo en el caso de que no supiesemos el puerto que utilizar siempre poniendo el valor 0 al principio.
+  });
